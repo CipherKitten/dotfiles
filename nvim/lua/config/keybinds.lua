39022414cefdabf8332246d10fc624343d9d4047
cipherkitten <wretchedfish@tutamail.com>
2025-09-10 21:37:42 +0000

    better

---
+++ nvim/lua/config/keybinds.lua
@@ -0,0 +1,12 @@
+vim.keymap.set("n", "<C-s>", ":w<cr>", { desc = "Save file"})
+vim.keymap.set("n", "<C-a>", ":wa<cr>", { desc = "Save all files"})
+vim.keymap.set("n", "<C-q>", ":q<cr>", { desc = "Quit" })
+vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<cr>", { desc = "Toggle file viewer" })
+-- Close the buffer you're currently on
+vim.keymap.set("n", "<A-q>", ":bd<CR>", { silent = true })
+
+vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>")
+vim.keymap.set("n", "<A-Tab>", ":BufferLineCyclePrev<CR>")
+
+
+
