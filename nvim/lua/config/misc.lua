793c7d29bfe431675ce3a5874c125e4ac981e6ce
cipherkitten <wretchedfish@tutamail.com>
2025-12-01 23:04:04 +0000

    u

--- nvim/lua/config/misc.lua
+++ nvim/lua/config/misc.lua
@@ -1,4 +1,18 @@
 vim.opt.number = true
+vim.opt.relativenumber = true
+-- Show relative numbers in normal mode, absolute in insert mode
+vim.api.nvim_create_autocmd({"InsertEnter"}, {
+  callback = function()
+    vim.opt.relativenumber = false
+  end
+})
+
+vim.api.nvim_create_autocmd({"InsertLeave"}, {
+  callback = function()
+    vim.opt.relativenumber = true
+  end
+})
+
 vim.opt.termguicolors = true
 require("bufferline").setup{}
 require('lualine').setup {
